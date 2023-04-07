f6ec56a3da497bec48372abfe8815419a6309bb6
namnguyenfinx <119911777+namnguyenfinx@users.noreply.github.com>
2023-04-07 08:00:02 +0700

    Update xss-code.js

--- xss-code.js
+++ xss-code.js
@@ -5,40 +5,22 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Add middleware for input validation and sanitization
-app.use((req, res, next) => {
-  if (req.body && req.body.comment) {
-    req.body.comment = sanitizeComment(req.body.comment);
-    next();
-  } else {
-    res.status(400).send("Bad Request");
-  }
-});
-
-// Add middleware for authentication and authorization
-app.use((req, res, next) => {
-  if (req.headers.authorization === "secret-token") {
-    next();
-  } else {
-    res.status(401).send("Unauthorized");
-  }
-});
-
 app.post("/comments", (req, res) => {
-  // Save the comment to the database with sanitized input
-  saveCommentToDatabase(req.body.comment);
+  const { comment } = req.body;
+
+  // Save the comment to the database without proper sanitization
+  saveCommentToDatabase(comment);
 
   res.send("Comment submitted successfully");
 });
 
 app.get("/comments", (req, res) => {
-  // Retrieve the comments from the database with sanitized output
+  // Retrieve the comments from the database without proper sanitization
   const comments = getCommentsFromDatabase();
-  const sanitizedComments = comments.map((comment) => sanitizeComment(comment));
 
   let html = "<h1>Comments</h1>";
-  for (const comment of sanitizedComments) {
-    html += `<p>${comment}</p>`;
+  for (const comment of comments) {
+    html += `<p>${comment.text}</p>`;
   }
 
   res.send(html);
@@ -48,7 +30,3 @@ app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
 
-// Function to sanitize comment input and output
-function sanitizeComment(comment) {
-  return comment.replace(/[^\w\s]/gi, "");
-}
